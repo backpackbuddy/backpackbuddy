1df05cb9335451f8f063a56a6b4592f419748f0f
yudi7ll <madewahyudi22@gmail.com>
2020-11-05 13:25:24 +0800

    add comment section

--- pages/premium-itinerary/[place].jsx
+++ pages/premium-itinerary/[place].jsx
@@ -17,6 +17,7 @@ import {
     Col,
     Container,
     Form,
+    Image,
     ListGroup,
     ListGroupItem,
     Row,
@@ -45,11 +46,11 @@ function Itinerary({ place }) {
                         <Carousel className="premium__carousel mb-4">
                             {
                                 data.map(({ foto }) => (
-                                    <Carousel.Item>
+                                    <Carousel.Item key={foto}>
                                         <img
                                             className="premium__img d-block w-100"
                                             src={foto}
-                                            alt="First slide"
+                                            alt={foto}
                                             loading="lazy"
                                         />
                                     </Carousel.Item>
@@ -102,21 +103,25 @@ function Itinerary({ place }) {
                                 </div>
 
                                 <div className="shadow-sm p-4 bg-white mt-4">
-                                    <div className="premium__comment">
+                                    <div className="premium__ulasan">
                                         <h3>Ulasan</h3>
                                         <Form action="#" method="POST">
-                                            <div className="premium__rating d-flex align-items-center">
-                                                <div className="text-warning font-weight-bold mr-1" style={{ fontSize: '3em' }}>{rating}</div>
-                                                <Rating
-                                                    className="my-4"
-                                                    emptySymbol={<StarOutlineIcon className="mx-1" height="2em" width="2em" />}
-                                                    fullSymbol={<StarFilledIcon className="mx-1" height="2em" width="2em" />}
-                                                    initialRating={rating}
-                                                    onClick={ratingClickHandler}
-                                                />
-                                                <div className="ml-2">75 Ulasan</div>
-                                            </div>
-                                            <Form.Group controlId="formBasicEmail">
+                                            <Form.Group>
+                                                <div className="mb-4 d-flex align-items-center flex-wrap">
+                                                    <div className="premium__rating d-flex flex-wrap align-items-center">
+                                                        <div className="text-warning mr-2" style={{ fontSize: '3em' }}>{rating}</div>
+                                                        <Rating
+                                                            className="mr-2"
+                                                            emptySymbol={<StarOutlineIcon className="mr-2" height="2em" width="2em" />}
+                                                            fullSymbol={<StarFilledIcon className="mr-2" height="2em" width="2em" />}
+                                                            initialRating={rating}
+                                                            onClick={ratingClickHandler}
+                                                        />
+                                                    </div>
+                                                    <div>74 Ulasan</div>
+                                                </div>
+                                            </Form.Group>
+                                            <Form.Group controlId="ulasan">
                                                 <Form.Control as="textarea" type="text" rows={4} placeholder="Tulis ulasan" />
                                             </Form.Group>
 
@@ -124,6 +129,45 @@ function Itinerary({ place }) {
                                                 Kirim Ulasan
                                             </Button>
                                         </Form>
+                                    </div>
+                                    <div className="premium__comments">
+                                        <hr/>
+                                        <div className="d-flex align-items-start">
+                                            <Image className="premium__avatar" src={data[1].foto} roundedCircle alt={data[1].foto} />
+                                            <div className="ml-4">
+                                                <div className="d-flex align-text-center flex-wrap justify-content-between">
+                                                    <h5>Lorem ipsum dolor</h5>
+                                                    <span>10 October 2020</span>
+                                                </div>
+                                                <h6>
+                                                    <Rating
+                                                        emptySymbol={<StarOutlineIcon className="mr-1" height="1em" width="1em" />}
+                                                        fullSymbol={<StarFilledIcon className="mr-1" height="1em" width="1em" />}
+                                                        initialRating={5}
+                                                        readonly
+                                                    />
+                                                </h6>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nesciunt obcaecati fugit aspernatur minus saepe accusamus velit eius consequuntur inventore harum quisquam maiores voluptatibus error odit, animi ducimus ipsum odio porro. Enim rerum voluptatem illo sint, temporibus sit assumenda quasi qui dolorem sed veritatis quis, ad quidem porro minus sapiente! Delectus veniam sed et suscipit labore aperiam eveniet! Enim, quod!</p>
+                                            </div>
+                                        </div>
+
+
+                                        <hr/>
+                                        <div className="d-flex align-items-start">
+                                            <Image className="premium__avatar" src={data[2].foto} roundedCircle alt={data[2].foto} />
+                                            <div className="ml-4">
+                                                <h5>John Doe</h5>
+                                                <h6>
+                                                    <Rating
+                                                        emptySymbol={<StarOutlineIcon className="mr-1" height="1em" width="1em" />}
+                                                        fullSymbol={<StarFilledIcon className="mr-1" height="1em" width="1em" />}
+                                                        initialRating={5}
+                                                        readonly
+                                                    />
+                                                </h6>
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, modi.</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </Col>
