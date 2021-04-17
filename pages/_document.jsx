5a6ed1848b39337b52e8c86f5aa6b9493578a70a
Yudi <madewahyudi22@gmail.com>
2021-04-17 21:19:00 +0800

    add roboto font to header

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,16 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import 'typeface-roboto';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps (ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
-  render() {
+  render () {
     return (
       <Html lang="in-ID" style={{ fontSize: '.9em' }}>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
