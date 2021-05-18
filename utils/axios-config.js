25cdb9eedd66d8a8bfdb4be7f3259f13e1ef9c04
Yudi <madewahyudi22@gmail.com>
2021-05-18 12:36:18 +0800

    update homepage attribute add Referrer-Policy header to axios

--- utils/axios-config.js
+++ utils/axios-config.js
@@ -9,6 +9,7 @@ function setAxiosConfig () {
 
 	axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 	axios.defaults.headers.post['Content-Type'] = 'application/json';
+	axios.defaults.headers['Referrer-Policy'] = 'origin-when-cross-origin';
 }
 
 export default setAxiosConfig;
