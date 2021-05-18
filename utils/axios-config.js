c0b3fed8efebbe37ea9607f7972092fbe715eca5
Yudi <madewahyudi22@gmail.com>
2021-05-18 21:58:15 +0800

    add cors axios header

--- utils/axios-config.js
+++ utils/axios-config.js
@@ -2,14 +2,15 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 
 function setAxiosConfig () {
-	const token = parseCookies().user_token
-	if (token) {
-		axios.defaults.headers.common.Authorization = `Bearer ${parseCookies().user_token}`
-	}
+  const token = parseCookies().user_token
+  if (token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${parseCookies().user_token}`
+  }
 
-	axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
-	axios.defaults.headers.post['Content-Type'] = 'application/json';
-	axios.defaults.headers['Referrer-Policy'] = 'origin-when-cross-origin';
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  axios.defaults.headers['Referrer-Policy'] = 'origin-when-cross-origin';
+  axios.defaults.headers['access-control-allow-origin'] = '*';
 }
 
 export default setAxiosConfig;
