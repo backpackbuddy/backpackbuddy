86b8f8a074924950d02ae67b07d64c1e051af057
Yudi <madewahyudi22@gmail.com>
2021-06-22 15:56:22 +0800

    minor

--- constants/sosmed-links.js
+++ constants/sosmed-links.js
@@ -1,8 +1,5 @@
-module.exports = {
-  facebookLink: 'https://www.facebook.com/backpackbuddyid',
-  messengerLink: 'https://m.me/backpackbuddyid',
-  whatsappLink: 'http://bit.ly/haichagiya',
-  instagramLink: 'https://www.instagram.com/backpack.buddy',
-  youtubeLink: 'https://www.youtube.com/channel/UCS9pAKRfIfMZz8nbCKORxHA',
-  linkedinLink: 'https://linkedin.com/in/backpackbuddy',
-};
+export const messengerLink = 'https://m.me/backpackbuddyid';
+export const whatsappLink = 'http://bit.ly/haichagiya';
+export const instagramLink = 'https://www.instagram.com/backpack.buddy';
+export const youtubeLink = 'https://www.youtube.com/channel/UCS9pAKRfIfMZz8nbCKORxHA';
+export const linkedinLink = 'https://linkedin.com/in/backpackbuddy';
