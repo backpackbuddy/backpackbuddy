d466c99acb5f3459bfdce6c77ad862967c763ee8
Yudi <madewahyudi22@gmail.com>
2021-07-16 15:14:31 +0800

    fix profile form not centered aligned

--- components/modules/ProfileInfoForm/index.jsx
+++ components/modules/ProfileInfoForm/index.jsx
@@ -111,7 +111,7 @@ function ProfileInfoForm() {
           controlId={`input${toTitleCase(name)}`}
         >
           <div className="col-12 col-sm-4">
-            <Form.Label>{label || toTitleCase(name)}</Form.Label>
+            <Form.Label className="mb-0">{label || toTitleCase(name)}</Form.Label>
           </div>
           <div className="col-12 col-sm-6">
             <Form.Control
